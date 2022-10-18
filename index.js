8ecb11be4b436702dc6cbb13c079167c36834669
Dito23 <89190087+anibolokadze@users.noreply.github.com>
2022-10-18 17:48:52 +0400

    input dom

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let bill = document.getElementById('bill-inp');
 let tipBtn = document.querySelectorAll('.tip');
+let people = document.getElementById('people-inp');
 
-let billValue = 0.0 //default
-
+// bill
+let billValue = 0.0; //default
 bill.addEventListener('input',setBillValue);
 
 function setBillValue(){
@@ -13,4 +14,20 @@ function setBillValue(){
     console.log(billValue)
 }
 
+// tip btns
 console.log(tipBtn[0].innerHTML);
+
+// number of people
+let peopleValue = 0; //default
+people.addEventListener('input', setPeopleValue);
+function setPeopleValue(){
+    peopleValue = parseFloat(people.value)
+    console.log(peopleValue);
+}
+
+
+// let qty = document.querySelector("input[name='number']");
+// qty.addEventListener("change", function (event) {
+//     if (this.value < 1) this.value = 1; // minimum is 1
+//     else this.value = Math.floor(this.value); // only integers allowed
+// })
