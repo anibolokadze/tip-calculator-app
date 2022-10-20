58e4aae6425ba4af79c72eff2920cfaea0755633
Dito23 <89190087+anibolokadze@users.noreply.github.com>
2022-10-20 15:14:33 +0400

    error message

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 let bill = document.getElementById('bill-inp');
 let tipBtn = document.querySelectorAll('.tip');
-let btnContainer = document.getElementsByClassName('.tips-wrapper');
 let currentBtn = document.getElementsByClassName("active");
-
 let people = document.getElementById('people-inp');
 let custom = document.getElementById('custom');
 let reset = document.getElementsByClassName('.reset-btn');
 let tipAmount = document.getElementById('tip-amount'); 
 let totalTip = document.getElementById('total-tip');
 
-
-bill.value = 0; //default
+let errorBlock = document.getElementById('error');
+let errorBorder = document.getElementById('people-inp')
+ //default
 bill.addEventListener('input', (e)=> {
     console.log(e.target.value);
-})
-
+});
 people.addEventListener('input', (e)=> {
     console.log(e.target.value);
+    lessThenZero();
 });
 
+
+
+const lessThenZero = () => {
+    if (+people.value === 0) {
+        errorBlock.textContent = "Can't be zero";
+        errorBorder.style.border = "2px solid red";
+        errorBlock.style.color = "red";
+        totalTip.textContent = "$0.00";
+        tipAmount.textContent = "$0.00";
+        people.value = "";
+    } else {
+        errorBlock.textContent = "";
+        errorBorder.style.border = "none";
+    }
+};
 
 function calculate(percent){
     if(people.value>0 && bill.value>0){
@@ -42,64 +56,12 @@ function customCalculate(customPercent){
 }
 customCalculate();
 
-
 // reset
-function resetColors() {
-    let j = 0;
-    for (j = 0; j < currentBtn.length - 1; j++) {
-      if (currentBtn[j].style.backgroundColor = "var(--strong-cyan)") {
-        currentBtn[j].style.backgroundColor = "var(--very-dark-cyan)";
-      }
-    }
-}
-function resetAll(resetButton) {
-    tipAmount.textContent = "$0.00";
-    totalTip.textContent = "$0.00";
+function resetAll() {
     bill.value = 0;
     people.value = 0;
     custom.value = 'Custom';
-    resetColors();
-};
+    tipAmount.textContent = "$0.00";
+    totalTip.textContent = "$0.00";
+}
 resetAll();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// add active class to current btn elements
-for (let i = 0; i < tipBtn.length; i++) {
-    tipBtn[i].addEventListener("click", function() {
-    // If there's no active class
-    if (currentBtn.length > 0) {
-      currentBtn[0].className = currentBtn[0].className.replace(" active", "");
-    }
-    // Add the active class to the current/clicked button
-    this.className += " active";
-  });
-};
-
-
-
-
-
-
-
-
-// let qty = document.querySelector("input[name='number']");
-// qty.addEventListener("change", function (event) {
-//     if (this.value < 1) this.value = 1; // minimum is 1
-//     else this.value = Math.floor(this.value); // only integers allowed
-// })
-
-
-
